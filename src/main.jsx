489b692a67086359595c0082b803ef271c92ecfa
gongyueetree <64692578+gongyueetree@users.noreply.github.com>
2026-09-11 10:21:06 +0800

    feat(v1.2): mount platform shell around existing agent

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import PlatformShell from './components/PlatformShell.jsx';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PlatformShell>
+      <App />
+    </PlatformShell>
   </React.StrictMode>
 );
